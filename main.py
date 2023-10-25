d3a17b9ecdb782fa6903e652d16fe707afb3a2a1
Janek1010 <janekdomozych99@gmail.com>
2023-10-25 17:27:31 +0200

    pracownicy.csv

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import csv
-import random
 import faker  # Biblioteka do generowania fałszywych danych
+from datetime import datetime, timedelta
+import random
 
 # Inicjalizacja generatora fałszywych danych
 fake = faker.Faker()
 
 # Struktura tabeli users
 # Możesz dostosować ją do swoich potrzeb
-fields = ['id', 'username', 'email', 'age']
+fields = ['id_pracownika_pk', 'data_urodzenia', 'data_zatrudnienia']
 
 # Ilość wierszy do wygenerowania
 num_rows = 100
 
 # Nazwa pliku CSV
-csv_file = 'fake_data.csv'
+csv_file = 'pracownicy.csv'
 
 # Otwieramy plik CSV do zapisu
 with open(csv_file, 'w', newline='') as csvfile:
@@ -24,11 +25,14 @@ with open(csv_file, 'w', newline='') as csvfile:
 
     # Generowanie nieprawdziwych danych i zapis do pliku
     for i in range(1, num_rows + 1):
+        birth_date = fake.date_of_birth(minimum_age=18, maximum_age=65)
+        # Generowanie losowej różnicy wieku między 20 a 40 lat
+        age_difference = random.randint(20, 40)
+        hire_date = birth_date + timedelta(days=365 * age_difference)
         fake_data = {
-            'id': i,
-            'username': fake.user_name(),
-            'email': fake.email(),
-            'age': random.randint(18, 65)
+            'id_pracownika_pk': i,
+            'data_urodzenia': birth_date.strftime('%Y-%m-%d'),
+            'data_zatrudnienia': hire_date.strftime('%Y-%m-%d'),
         }
         writer.writerow(fake_data)
 
