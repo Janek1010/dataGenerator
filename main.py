1bce4c1be2980e05f20009813e426b070a81cadf
Janek1010 <janekdomozych99@gmail.com>
2023-10-25 17:41:22 +0200

    kasy

--- main.py
+++ main.py
@@ -3,31 +3,18 @@ import faker  # Biblioteka do generowania fałszywych danych
 from datetime import datetime, timedelta
 import random
 
-# Inicjalizacja generatora fałszywych danych
+
 fake = faker.Faker()
-
-# Bieżący rok
 current_year = 2023
-
-# Struktura tabeli users
-# Możesz dostosować ją do swoich potrzeb
-fields = ['id_pracownika_pk', 'data_urodzenia', 'data_zatrudnienia']
-
-# Ilość wierszy do wygenerowania
-num_rows = 100
-
-# Nazwa pliku CSV
+pracownicy = ['id_pracownika_pk', 'data_urodzenia', 'data_zatrudnienia']
+ilosc_pracownikow = 100
 csv_file = 'pracownicy.csv'
 
-# Otwieramy plik CSV do zapisu
 with open(csv_file, 'w', newline='') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=fields)
-
-    # Tworzenie nagłówków kolumn
+    writer = csv.DictWriter(csvfile, fieldnames=pracownicy)
     writer.writeheader()
 
-    # Generowanie nieprawdziwych danych i zapis do pliku
-    for i in range(1, num_rows + 1):
+    for i in range(1, ilosc_pracownikow + 1):
         birth_date = fake.date_of_birth(minimum_age=18, maximum_age=65)
         # Generowanie losowej różnicy wieku między 20 a 40 lat
         age_difference = random.randint(20, 40)
@@ -38,10 +25,24 @@ with open(csv_file, 'w', newline='') as csvfile:
             hire_date = datetime(current_year, 12, 31)
 
         fake_data = {
-            'id_pracownika_pk': i,
-            'data_urodzenia': birth_date.strftime('%Y-%m-%d'),
-            'data_zatrudnienia': hire_date.strftime('%Y-%m-%d'),
+            pracownicy[0]: i,
+            pracownicy[1]: birth_date.strftime('%Y-%m-%d'),
+            pracownicy[2]: hire_date.strftime('%Y-%m-%d'),
         }
         writer.writerow(fake_data)
 
-print(f'Wygenerowano {num_rows} wierszy danych i zapisano je w pliku {csv_file}')
+
+stanowisko_kasowe = ['id_kasy_pk', 'numer_kasy']
+ilosc_kas = 10
+csv_file = 'stanowisko_kasowe.csv'
+
+with open(csv_file, 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=stanowisko_kasowe)
+    writer.writeheader()
+
+    for i in range(1, ilosc_kas + 1):
+        fake_data = {
+            stanowisko_kasowe[0]: i,
+            stanowisko_kasowe[1]: i,
+        }
+        writer.writerow(fake_data)
