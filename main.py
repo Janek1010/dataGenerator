9447dc05723248929f60df2a6a6c6e18e8c3323b
Janek1010 <janekdomozych99@gmail.com>
2023-10-25 17:58:37 +0200

    kasy

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ import faker  # Biblioteka do generowania fałszywych danych
 from datetime import datetime, timedelta
 import random
 
+ilosc_kas = 10
+ilosc_pracownikow = 100
+ilosc_obslug = 1000
 
 fake = faker.Faker()
 current_year = 2023
-pracownicy = ['id_pracownika_pk', 'data_urodzenia', 'data_zatrudnienia']
-ilosc_pracownikow = 100
-csv_file = 'pracownicy.csv'
 
+pracownicy = ['id_pracownika_pk', 'data_urodzenia', 'data_zatrudnienia']
+stanowisko_kasowe = ['id_kasy_pk', 'numer_kasy']
+obsluga = ['id_pk', 'id_wniosku_fk', 'id_pracownika_fk', 'id_stanowiska_fk']
+
+
+
+csv_file = 'pracownicy.csv'
 with open(csv_file, 'w', newline='') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=pracownicy)
     writer.writeheader()
@@ -32,8 +39,6 @@ with open(csv_file, 'w', newline='') as csvfile:
         writer.writerow(fake_data)
 
 
-stanowisko_kasowe = ['id_kasy_pk', 'numer_kasy']
-ilosc_kas = 10
 csv_file = 'stanowisko_kasowe.csv'
 
 with open(csv_file, 'w', newline='') as csvfile:
@@ -44,5 +49,20 @@ with open(csv_file, 'w', newline='') as csvfile:
         fake_data = {
             stanowisko_kasowe[0]: i,
             stanowisko_kasowe[1]: i,
+        }
+        writer.writerow(fake_data)
+
+csv_file = 'obsluga.csv'
+
+with open(csv_file, 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=obsluga)
+    writer.writeheader()
+
+    for i in range(1, ilosc_obslug + 1):
+        fake_data = {
+            obsluga[0]: i,
+            obsluga[1]: i,
+            obsluga[2]: i % ilosc_pracownikow,
+            obsluga[3]: i % ilosc_kas,
         }
         writer.writerow(fake_data)
